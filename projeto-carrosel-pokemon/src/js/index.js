c03ee147cfad7eed5571cc6d2a1dd9f4bbb48832
js-johnsora <108762037+js-johnsora@users.noreply.github.com>
2022-08-21 22:25:10 -0300

    Add files via upload

---
+++ projeto-carrosel-pokemon/src/js/index.js
@@ -0,0 +1,30 @@
+//console.log(document.getElementById(btn-avancar));
+const btnAvancar = document.getElementById('btn-avancar');
+const btnVoltar = document.getElementById('btn-voltar');
+const cartoes = document.querySelectorAll('.cartao');
+let cartaoAtual = 0;
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector('.selecionado');
+    cartaoSelecionado.classList.remove("selecionado");
+}
+
+function mostrarCartao(indiceCartao) {
+    cartoes[indiceCartao].classList.add('selecionado');
+}
+btnAvancar.addEventListener('click', function () {
+    if(cartaoAtual === cartoes.length -1) return;
+
+    esconderCartaoSelecionado();
+    cartaoAtual++;
+    cartoes[cartaoAtual].classList.add('selecionado');
+});
+
+
+btnVoltar.addEventListener('click', function () {
+    if(cartaoAtual === 0) return;
+
+    esconderCartaoSelecionado();
+    cartaoAtual--;
+    cartoes[cartaoAtual].classList.add('selecionado');
+});
